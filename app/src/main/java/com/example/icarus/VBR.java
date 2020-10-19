77756cfab962d58c9d340beb27fb5eadefb618bb
qiikaii <sharpnshiny22@gmail.com>
2020-10-19 20:34:39 +0800

    VBR DONE BOYS

--- app/src/main/java/com/example/icarus/VBR.java
+++ app/src/main/java/com/example/icarus/VBR.java
@@ -2,7 +2,6 @@ package com.example.icarus;
 
 public class VBR {
 
-    private long VBRSector;
     private String OEM;
     private long bytesPerSector;
     private long sectorsPerCluster;
@@ -13,18 +12,12 @@ public class VBR {
     private String mediaType;
     private long offset; // Number of sectors before start of partition
     private long bit32Sectors;
+    private long bit32SectorsOfFat;
     private long rootCluster;
     private long FSInfoSector;
 
     public VBR() {
     }
-
-//    public VBR(Partition partition) {
-//        VBRSector = partition.getStartOfPartition();
-//    }
-
-
-    public void setVBRSector(Long startOfPartition) { long VBRSector = startOfPartition; }
 
     public void setOEM(String hexData) { this.OEM = hexData; }
 
@@ -56,14 +49,55 @@ public class VBR {
 
     public void setBit32Sectors(long bit32Sectors) { this.bit32Sectors = bit32Sectors; }
 
+    public void setBit32SectorsOfFat(long bit32SectorsOfFat) { this.bit32SectorsOfFat = bit32SectorsOfFat; }
+
     public void setRootCluster(long rootCluster) { this.rootCluster = rootCluster; }
 
     public void setFSInfoSector(long FSInfoSector) { this.FSInfoSector = FSInfoSector; }
 
     public String getOEM() { return this.OEM; }
 
-    public long getVBRSector() { return VBRSector; }
+    public long getBytesPerSector() { return bytesPerSector; }
 
+    public long getSectorsPerCluster() { return sectorsPerCluster; }
+
+    public long getReservedAreaSize() { return reservedAreaSize; }
+
+    public long getNumOfFats() { return numOfFats; }
+
+    public long getBit16Sectors() { return bit16Sectors; }
+
+    public long getMaxRootFiles() { return maxRootFiles; }
+
+    public String getMediaType() { return mediaType; }
+
+    public long getOffset() { return offset; }
+
+    public long getBit32Sectors() { return bit32Sectors; }
+
+    public long getBit32SectorsOfFat() { return bit32SectorsOfFat; }
+
+    public long getRootCluster() { return rootCluster; }
+
+    public long getFSInfoSector() { return FSInfoSector; }
+
+    public String getVBRInfo() {
+        String VBRInfo = ("OEM: " + this.OEM
+                + "\nBytes Per Sector: " + this.bytesPerSector
+                + "\nSectors Per Cluster: " + this.sectorsPerCluster
+                + "\nReserved Area Size (in sector)" + this.reservedAreaSize
+                + "\nNumber of Fats: " + this.numOfFats
+                + "\n16-bit value of number of sectors: " + this.bit16Sectors
+                + "\nMedia Type: " + this.mediaType
+                + "\nNumber of sectors before start of partition: " + this.offset
+                + "\n32-bit value of number of sectors: " + this.bit32Sectors
+                + "\n32-bit value of 1 FAT: " + this.bit32SectorsOfFat
+                + "\nCluster of Root Director: " + this.rootCluster
+                + "\nSector of FSINFO: " + this.FSInfoSector
+        );
+
+        return VBRInfo;
+    }
 
 }
 
