58c2a2c3a86dbd2a3269d3148d0b22d530793685
qiikaii <sharpnshiny22@gmail.com>
2020-10-19 12:51:14 +0800

    Replaced with switch cases for "faster" analysis

--- app/src/main/java/com/example/icarus/Partition.java
+++ app/src/main/java/com/example/icarus/Partition.java
@@ -11,66 +11,61 @@ public class Partition {
     private Long endOfPartition;
     private Long lenOfPartition;
 
+    private String hexDataString;
+
     public Partition() {
     }
 
     public void setBootableStatus(StringBuilder hexData) {
-        if (hexData.toString().equals("80")) {
-            this.bootableStatus = "Bootable";
-        }
 
-        if (hexData.toString().equals("00")) {
-            this.bootableStatus = "Non-Bootable";
+        switch(hexDataString = hexData.toString()) {
+            case "80":
+                this.bootableStatus = "Bootable";
+                break;
+            case "00":
+                this.bootableStatus = "Non-Bootable";
+                break;
         }
     }
 
     public void setPartitionType(StringBuilder hexData) {
-        if (hexData.toString().equals("00")) {
-            this.partitionType = "Empty";
-        }
 
-        else if (hexData.toString().equals("01")) {
-            this.partitionType = "FAT12";
-        }
-
-        else if (hexData.toString().equals("02")) {
-            this.partitionType = "XENIX root";
-        }
-
-        else if (hexData.toString().equals("03")) {
-            this.partitionType = "XENIX usr";
-        }
-
-        else if (hexData.toString().equals("04")) {
-            this.partitionType = "FAT16 <32M";
-        }
-
-        else if (hexData.toString().equals("05")) {
-            this.partitionType = "Extended";
-        }
-
-        else if (hexData.toString().equals("06")) {
-            this.partitionType = "FAT16";
-        }
-
-        else if (hexData.toString().equals("0A")) {
-            this.partitionType = "OS/2 Boot Manager";
-        }
-
-        else if (hexData.toString().equals("0B")) {
-            this.partitionType = "W95 FAT32";
-        }
-
-        else if (hexData.toString().equals("0C")) {
-            this.partitionType = "W95 FAT32 (LBA)";
-        }
-
-        else if (hexData.toString().equals("EE")) {
-            this.partitionType = "GPT";
-        }
-
-        else {
-            this.partitionType = "Invalid Partition Type";
+        switch(hexDataString = hexData.toString()) {
+            case "00":
+                this.partitionType = "Empty";
+                break;
+            case "01":
+                this.partitionType = "FAT12";
+                break;
+            case "02":
+                this.partitionType = "XENIX root";
+                break;
+            case "03":
+                this.partitionType = "XENIX usr";
+                break;
+            case "04":
+                this.partitionType = "FAT16 <32M";
+                break;
+            case "05":
+                this.partitionType = "Extended";
+                break;
+            case "06":
+                this.partitionType = "FAT16";
+                break;
+            case "0A":
+                this.partitionType = "OS/2 Boot Manager";
+                break;
+            case "0B":
+                this.partitionType = "W95 FAT32";
+                break;
+            case "0C":
+                this.partitionType = "W95 FAT32 (LBA)";
+                break;
+            case "EE":
+                this.partitionType = "GPT";
+                break;
+            default:
+                this.partitionType = "Invalid Partition Type";
         }
     }
 
