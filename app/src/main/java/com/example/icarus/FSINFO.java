b6fc5a98b2353d4561e909634c210e3be1b08d2e
Xracius <andychua23@hotmail.com>
2020-10-19 17:39:02 +0800

    Tadsda

--- app/src/main/java/com/example/icarus/FSINFO.java
+++ app/src/main/java/com/example/icarus/FSINFO.java
@@ -1,13 +1,13 @@
 package com.example.icarus;
 
-public class FSINFO {
+public class FSInfo {
     private String FSINOSignature;
     private String localSignature;
     private String lastKnownFreeCluster;
     private String nextFreeCluster;
     private String trailingSignature;
 
-    public FSINFO() {
+    public FSInfo() {
     }
 
     public void setFSINOSignature(String FSINOSignature) {
@@ -50,4 +50,12 @@ public class FSINFO {
         return trailingSignature;
     }
 
+    public Boolean chkFSINFOValidity(){
+        if (this.getFSINOSignature().equals("41615252")&& this.getTrailingSignature().equals("0000AA55")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
